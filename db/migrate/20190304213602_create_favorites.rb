0b1764233fe46f947d5bb9d1d87dc32b1754c518
Victoria Mei <vm930@hotmail.com>
2019-03-04 16:50:02 -0500

    finish creating tables for migration db

--- db/migrate/20190304213602_create_favorites.rb
+++ db/migrate/20190304213602_create_favorites.rb
@@ -1,4 +1,11 @@
 class CreateFavorites < ActiveRecord::Migration[5.0]
   def change
+    create_table :favorites do |t|
+     t.interger :user_id
+     t.interger :brewery_id 
+     t.interger :rating
+
+     t.timestamps
+    end 
   end
 end
--- db/migrate/20190304213630_create_breweries.rb.rb
+++ db/migrate/20190304213630_create_breweries.rb.rb
@@ -1,4 +1,16 @@
 class CreateBreweries.rb < ActiveRecord::Migration[5.0]
   def change
+    create_table :breweries do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :country
+      t.string :phone
+      t.string :website_url
+     
+      t.timestamps
+    end 
   end
 end
