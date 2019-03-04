4649237b9f2fdc3c34abc6e88528cbe0943c7308
Victoria Mei <vm930@hotmail.com>
2019-03-04 16:42:29 -0500

    create migration files from Rakefile

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
     has_many :favorite
-
     has_many :brewery, :through => :favorite
 end
---
+++ db/migrate/20190304213630_create_breweries.rb.rb
@@ -0,0 +1,4 @@
+class CreateBreweries.rb < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
